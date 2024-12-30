9a70b5323c47d53c8bd8cdd338f94640785e1d1a
rajuboppa <rajupatelboppa@gmail.com>
2024-12-29 23:24:41 -0500

    Adding a progress bar for the delete modal timer

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,10 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+const TIMER = 2000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prev) => prev - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
     console.log("timer set");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 2000);
+    }, TIMER);
     return () => {
       console.log("clearing timer..");
       clearTimeout(timer);
@@ -23,6 +34,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
